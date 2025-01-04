919e0c162b474e43c4dadcbbf8e28469521f8501
TOGoS <togos00@gmail.com>
2025-01-04 13:42:30 -0600

    Configurable signal handlers
    All well and good, but there's a problem:
    Deno blocks on writes to FIFOs.
    
    Not sure if FIFOs can be opened/written-to
    non-blocklingly in any lanugage, but
    maybe I'll try writing a thing in C
    to see if it's possible.

--- devsim.ts
+++ devsim.ts
@@ -1,15 +1,72 @@
 import { ensureDir, ensureFile } from 'https://deno.land/std@0.224.0/fs/mod.ts';
 import { delay } from 'https://deno.land/std@0.224.0/async/mod.ts';
 
-const dir = Deno.args[0];
-if (!dir) {
-	console.error(
-		'Please provide a directory where the fake devices should be created.',
-	);
-	Deno.exit(1);
+type Command = AndCommand | CleanupCommand | ExitCommand;
+type CleanupCommand = {
+	commandType: 'cleanup';
+};
+type ExitCommand = {
+	commandType: 'exit';
+	exitCode: number;
+};
+type AndCommand = {
+	commandType: 'and';
+	subcommands: Command[];
+};
+
+function parseCommand(command: string): Command {
+	command = command.trim();
+	const andParts = command.split('&&');
+	if (andParts.length > 1) {
+		return {
+			commandType: 'and',
+			subcommands: andParts.map(parseCommand),
+		};
+	}
+	const cmdArgs = command.split(' ').map((s) => s.trim());
+	if (cmdArgs[0] === 'cleanup') return { commandType: 'cleanup' };
+	if (cmdArgs[0] === 'exit') {
+		return { commandType: 'exit', exitCode: parseInt(cmdArgs[1]) };
+	}
+	throw new Error(`Invalid command: ${cmdArgs[0]}`);
 }
 
-await ensureDir(dir);
+async function runCommand(command: Command) {
+	info(`Running command: ${JSON.stringify(command)}`);
+	if (command.commandType === 'cleanup') {
+		await cleanup();
+	} else if (command.commandType === 'exit') {
+		Deno.exit(command.exitCode);
+	} else if (command.commandType === 'and') {
+		for (const subcommand of command.subcommands) {
+			await runCommand(subcommand);
+		}
+	}
+}
+
+type FilePath = string;
+
+let onTerm: Command = parseCommand('cleanup && exit 1');
+let onInt: Command = parseCommand('cleanup && exit 1');
+const eventSimDirs: FilePath[] = [];
+
+for (const arg of Deno.args) {
+	let m: RegExpMatchArray | null;
+	if ((m = /^--on-term=(\d+)$/.exec(arg)) !== null) {
+		onTerm = parseCommand(m[1]);
+	} else if ((m = /^--on-int=(\d+)$/.exec(arg)) !== null) {
+		onInt = parseCommand(m[1]);
+	} else if ((m = /^--event-sim-dir=(.+)$/.exec(arg)) !== null) {
+		eventSimDirs.push(m[1]);
+	} else {
+		console.error(`Unrecognized option: ${arg}`);
+		Deno.exit(1);
+	}
+}
+
+for (const dir of eventSimDirs) {
+	await ensureDir(dir);
+}
 
 const devices: Record<string, boolean> = {};
 let running = true;
@@ -41,12 +98,14 @@ async function createDevice(
 	name: string,
 	simulatedDeviceType: 'WiiBalanceBoard' | 'Empty',
 ) {
+	const dir = eventSimDirs[Math.floor(Math.random() * eventSimDirs.length)];
 	const devicePath = `${dir}/${name}`;
 	try {
 		await createFifo(devicePath);
 		devices[devicePath] = true;
-		info(`Created device: ${devicePath}`);
+		info(`Created ${simulatedDeviceType} device: ${devicePath}`);
 		if (simulatedDeviceType === 'WiiBalanceBoard') {
+			info(`Starting WiiBalanceBoard simulator for ${devicePath}...`);
 			writeToDevice(devicePath);
 		}
 	} catch (err) {
@@ -67,19 +126,32 @@ async function removeDevice(devicePath: string) {
 }
 
 async function writeToDevice(devicePath: string) {
-	while (devices[devicePath] && running) {
-		const data = new Uint8Array(24);
-		crypto.getRandomValues(data);
+	info(`Starting writing to device at ${devicePath}...`);
+	try {
+		const writeStream = await Deno.open(devicePath, { write: true });
 		try {
-			await Deno.writeFile(devicePath, data);
-		} catch (err) {
-			throw new AggregateError(
-				[err],
-				`Error writing to device at ${devicePath}`,
-			);
+			while (devices[devicePath] && running) {
+				const data = new Uint8Array(24);
+				crypto.getRandomValues(data);
+				try {
+					info(`Writing ${data.length} bytes to ${devicePath}...`);
+					await writeStream.write(data);
+					info(`wrote ${data.length} bytes to ${devicePath}`);
+				} catch (err) {
+					throw new AggregateError(
+						[err],
+						`Error writing to device at ${devicePath}`,
+					);
+				}
+				await delay(1000);
+			}
+		} finally {
+			await writeStream.close();
 		}
-		await delay(1000);
+	} finally {
+		await Deno.remove(devicePath);
 	}
+	info(`Stopped writing to device at ${devicePath}`);
 }
 
 async function simulateDevices() {
@@ -102,20 +174,20 @@ async function simulateDevices() {
 }
 
 async function cleanup() {
+	running = false;
 	info('Cleaning up...');
 	for (const devicePath in devices) {
 		await removeDevice(devicePath);
 	}
 }
 
-Deno.addSignalListener('SIGTERM', () => {
+Deno.addSignalListener('SIGTERM', async () => {
 	info('Received SIGTERM, stopping...');
-	running = false;
+	await runCommand(onTerm);
+});
+Deno.addSignalListener('SIGINT', async () => {
+	info('Received SIGINT, stopping...');
+	await runCommand(onInt);
 });
 
-try {
-	await simulateDevices();
-} finally {
-	await cleanup();
-	Deno.exit(0);
-}
+await simulateDevices();

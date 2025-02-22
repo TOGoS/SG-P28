d9a9f3dbfa41a7c49db84873f04214bb2a520ef3
TOGoS <togos00@gmail.com>
2025-02-22 19:16:55 +0000

    Refactor dbusdemo.ts using more ProcessLikes

--- dbusdemo.ts
+++ dbusdemo.ts
@@ -1,7 +1,10 @@
 import { DBus, SystemDBus } from 'npm:@clebert/node-d-bus@1.0.0';
 import { Adapter, Device } from 'npm:@clebert/node-bluez@1.0.0';
+import ProcessLike, { ProcSig } from './src/main/ts/process/ProcessLike.ts';
 
 import * as dbusTypes from 'npm:d-bus-type-system@1.0.0';
+import { EXITCODE_ABORTED, functionToProcessLike, newPseudoPid } from './src/main/ts/process/util.ts';
+import { ProcessGroup } from './src/main/ts/process/ProcessGroup.ts';
 
 // Based on code from https://github.com/clebert/node-bluez
 
@@ -11,14 +14,29 @@ function usleep(duration:number) : Promise<void> {
 	});
 }
 
-class WBBConnector {
+class WBBConnector implements ProcessLike {
 	#adapter : Adapter;
 	#devices : Device[];
-	constructor(adapter : Adapter) {
+	#abortSignal : AbortSignal;
+	#abortController : AbortController;
+	#done : Promise<number>;
+	#id : string;
+	constructor(adapter : Adapter, opts : {id?:string} = {}) {
+		this.#id = opts.id || newPseudoPid();
 		this.#adapter = adapter;
 		this.#devices = [];
+		this.#abortController = new AbortController();
+		this.#abortSignal = this.#abortController.signal;
+		this.#done = new Promise((resolve,reject) => {
+			this.#abortSignal.addEventListener('abort', () => {
+				for( const dev of this.#devices ) {
+					dev.disconnect();
+				}		
+				resolve(EXITCODE_ABORTED);
+			});
+		});
 	}
-	async connectTo(macAddr : string ) {
+	async connectTo(macAddr : string ) : Promise<Device> {
 		console.log("Waiting for device...");
 		let device = await this.#adapter.waitForDevice(macAddr);
 		
@@ -40,13 +58,39 @@ class WBBConnector {
 		
 		await device.callMethod("Connect");
 		console.log("Connected!");
+		
+		return device;
 	}
-	close() {
-		for( const dev of this.#devices ) {
-			dev.disconnect();
-		}
-	}
+	
+	get id() { return this.#id; }
+	kill(sig:ProcSig) { this.#abortController.abort(); }
+	wait(): Promise<number> { return this.#done; }
 }
+
+function startConnectLoop1(macAddr : string, con : ()=>Promise<Device> ) : ProcessLike {
+	return functionToProcessLike(async (sig) => {
+		while( !sig.aborted ) {
+			try {
+				const device = await con();
+				while( !sig.aborted && await device.isConnected() ) {
+					console.log(`${macAddr} still connected`);
+					await usleep(1000);
+				}
+			} catch( e ) {
+				console.error(`Error during connection to ${macAddr}: ${e}`);
+				console.error(`Sleeping a bit, then will attempt to re-connect`);
+				await usleep(1000);
+			}
+		}
+		console.log(`Connection loop to ${macAddr} exiting.`);
+		return EXITCODE_ABORTED;
+	});
+}
+
+function startConnectLoop2(macAddr : string, connector : WBBConnector) : ProcessLike {
+	return startConnectLoop1(macAddr, () => connector.connectTo(macAddr));
+}
+
 
 async function main() {
 	const dBus = new SystemDBus();
@@ -68,11 +112,13 @@ async function main() {
 			await adapter.setPowered(true);
 			await adapter.startDiscovery();
 			
-			await connector.connectTo('00:21:BD:D1:5C:A9');
-			console.log("Woohoo, connected!");
-			await usleep(5000);
+			startConnectLoop2('00:21:BD:D1:5C:A9', connector);
+			
+			
+			
+			await connector.wait();
 		} finally {
-			connector.close();
+			connector.kill('SIGTERM');
 			
 			unlockAdapter();
 		}

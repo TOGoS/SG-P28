e2bebed191f6d028cd4fd2e17709b951fefcdd1b
TOGoS <togos00@gmail.com>
2025-04-11 17:28:43 +0000

    udplisten takes [host:]port to listen on as argument

--- udplisten.ts
+++ udplisten.ts
@@ -7,11 +7,25 @@ function uint8ArrayToHex(data:Uint8Array) : string {
 	return hexes.join('');
 }
 
+let hostname = "127.0.0.1";
+let port = 9901;
+
+for( const arg of Deno.args ) {
+	let m : RegExpExecArray|null;
+	if( (m = /^(?:\[(?<hostname>[^\]]+)\]|(?<hostname>[^\[\]:]+)):(?<port>\d+)$/.exec(arg)) != null ) {
+		hostname = m.groups!['hostname'];
+		port = +m.groups!['port'];
+	} else if( (m = /^(\d+)$/.exec(arg)) != null ) {
+		port = +m[1];
+	} else {
+		throw new Error(`Unrecognized argument: ${arg}`);
+	}
+}
 
 const listener = Deno.listenDatagram({
 	transport: "udp",
-	hostname: "127.0.0.1",
-	port: 9901,
+	hostname,
+	port,
 });
 while( true ) {
 	const [data, sourceAddr] = await listener.receive();

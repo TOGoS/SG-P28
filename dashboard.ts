7a3f2c61ab712da093f9899c4356e63bbacd8332
TOGoS <togos00@gmail.com>
2025-09-19 15:09:57 -0500

    Use TUIAppFramework3 to draw dashboard'

--- dashboard.ts
+++ dashboard.ts
@@ -1,14 +1,17 @@
-import * as ansicodes from 'https://deno.land/x/tui@2.1.11/src/utils/ansi_codes.ts';
-import { MqttClient, MqttPackets } from "jsr:@ymjacky/mqtt5@0.0.19";
-import { inputEvents } from 'https://deno.land/x/scratch38s15@0.0.8/src/lib/ts/terminput/inputeventparser.ts';
-import TOGTUICanvas, { TOGTUIRenderer } from 'https://deno.land/x/scratch38s15@0.0.8/src/lib/ts/termdraw/TOGTUICanvas.ts';
-import BoxDrawr from 'https://deno.land/x/scratch38s15@0.0.6/src/lib/ts/termdraw/BoxDrawr.ts';
-import { formatTargetSpec, parseTargetSpec, TargetSpec } from "./src/main/ts/sink/sinkspec.ts";
-import { PossiblyTUIAppSpawner } from 'https://deno.land/x/scratch38s15@0.0.14/src/lib/ts/tuiappframework3.ts';
-import { DenoStdinLike } from '../Scratch38-S0015/src/lib/ts/tuiappframework3.ts';
+import * as ansi from 'https://deno.land/x/scratch38s15@0.0.14/src/lib/ts/termdraw/ansi.ts';
+import { SizedRasterable } from 'https://deno.land/x/scratch38s15@0.0.14/src/lib/ts/termdraw/components2.ts';
+import TextRaster2 from 'https://deno.land/x/scratch38s15@0.0.14/src/lib/ts/termdraw/TextRaster2.ts';
+import { createUniformRaster, drawTextToRaster } from 'https://deno.land/x/scratch38s15@0.0.14/src/lib/ts/termdraw/textraster2utils.ts';
+import Vec2D from 'https://deno.land/x/scratch38s15@0.0.14/src/lib/ts/termdraw/Vec2D.ts';
+import { vec2dsAreEqual } from 'https://deno.land/x/scratch38s15@0.0.14/src/lib/ts/termdraw/vecutils.ts';
+import { DenoStdinLike, PossiblyTUIAppContext, PossiblyTUIAppSpawner, runTuiApp, TUIAppRunnerContext, Waitable } from 'https://deno.land/x/scratch38s15@0.0.14/src/lib/ts/tuiappframework3.ts';
+import WatchableVariable, { makeReadonlyWatchable } from 'https://deno.land/x/scratch38s15@0.0.14/src/lib/ts/WatchableVariable.ts';
+import { MqttClient } from "jsr:@ymjacky/mqtt5@0.0.19";
+import KeyEvent from '../Scratch38-S0015/src/lib/ts/terminput/KeyEvent.ts';
+import { AbstractAppInstance } from '../Scratch38-S0015/src/lib/ts/tuiappframework3.ts';
 import ProcessLike from './src/main/ts/process/ProcessLike.ts';
-import { Waitable } from '../Scratch38-S0015/src/lib/ts/tuiappframework3.ts';
 import { functionToProcessLike } from './src/main/ts/process/util.ts';
+import { formatTargetSpec, parseTargetSpec, TargetSpec } from "./src/main/ts/sink/sinkspec.ts";
 
 const textDecoder = new TextDecoder();
 const textEncoder = new TextEncoder();
@@ -20,35 +23,42 @@ type ConnectionStatus<T> = {
 	target: T
 }
 
-class Dashboard {
-	#canvas : TOGTUICanvas;
+class Dashboard implements SizedRasterable {
+	#ctx : PossiblyTUIAppContext
 	#needClear : boolean = true;
 	#connectionStatus : ConnectionStatus<TargetSpec> = {"status":"not-connected"};
 	#attrMap : Map<string,string> = new Map();
 	#logMessages : string[] = [];
 	#screenSize : {columns:number, rows:number} = {rows: 40, columns: 80};
 	
-	constructor(out:WritableStreamDefaultWriter) {
-		this.#canvas = new TOGTUICanvas(out, this.render.bind(this));
+	constructor(ctx:PossiblyTUIAppContext) {
+		this.#ctx = ctx;
 	}
-	get canvas() { return this.#canvas; }
+	
+	rasterForSize(size : Vec2D<number>) : TextRaster2 {
+		let rast = createUniformRaster(size, "/", "");
+		rast = drawTextToRaster(rast, {x:1,y:1}, " "+this._connectionStatusAsString+" ", ansi.BRIGHT_WHITE_TEXT );
+		
+		for( let y=size.y-2, i=this.#logMessages.length - 1; y >= 3 && i >= 0; --i, --y ) {
+			rast = drawTextToRaster(rast, {x:1, y}, " "+this.#logMessages[i]+" ", "");
+		}
+		
+		return rast;
+	}
+	
+	_requestRedraw() {
+		this.#ctx.setScene(this);
+	}
 	
 	set connectionStatus(status:ConnectionStatus<TargetSpec>) {
 		// throw new Error(`Setting connection status to ${JSON.stringify(status)}!`);
 		this.#connectionStatus = status;
-		// this.log(`Connection status updated to ${JSON.stringify(status)}, or ${this._connectionStatusAsString}`);
-		this.#canvas.requestRedraw();
+		this._requestRedraw();
 	}
 	
-	enterTui() {
-		this.#canvas.enterTui();
-	}
-	exitTui() {
-		this.#canvas.exitTui();
-	}
 	update(key:string, value:Uint8Array) {
 		this.#attrMap.set(key, textDecoder.decode(value));
-		this.#canvas.requestRedraw();
+		this._requestRedraw();
 	}
 	
 	#renderCount = 0;
@@ -61,30 +71,13 @@ class Dashboard {
 		}
 	}
 	
-	async render(out:WritableStreamDefaultWriter) : Promise<void> {
-		this.#renderCount++;
-		if( this.#needClear ) {
-			this.#needClear = false;
-			await out.write(textEncoder.encode(ansicodes.CLEAR_SCREEN));
-		}
-		out.write(textEncoder.encode(ansicodes.moveCursor(0,0)));
-		out.write(textEncoder.encode(this.#renderCount + " | " + this._connectionStatusAsString))
-		out.write(textEncoder.encode(ansicodes.moveCursor(1,0)));
-		for( const [k,v] of this.#attrMap.entries() ) {
-			out.write(textEncoder.encode(`${k} : ${v}\n`));
-		}
-		for( const msg of this.#logMessages ) {
-			out.write(textEncoder.encode(msg+"\n"));
-		}
-		return Promise.resolve();
-	}
 	log(text:string) {
 		this.#logMessages.push(text);
-		this.#canvas.requestRedraw();
+		this._requestRedraw();
 	}
 	set screenSize(size:{rows:number, columns:number}) {
 		this.#screenSize = size;
-		this.#canvas.requestRedraw();
+		this._requestRedraw();
 	}
 }
 
@@ -110,73 +103,138 @@ function functionToProcessLikeSpawner(fn:(this:ProcessLike, ctx:ProcessLikeSpawn
 	}
 }
 
-const dashboardMain = (sourceSpec:TargetSpec) => async (ctx:ProcessLikeSpawnContext, outerAbortSignal:AbortSignal) : Promise<number> => {
-	const out = ctx.stdout.writable.getWriter();
-	const dashboard = new Dashboard(out);
-	
-	const abortController = new AbortController();
-	const abortSignal = abortController.signal;
-	
-	outerAbortSignal.addEventListener("abort", () => abortController.abort());
-	
-	// dashboard.log("Connecting to "+formatTargetSpec(sourceSpec));
-	dashboard.connectionStatus = {status: "connecting", target: sourceSpec };
-	
-	if( sourceSpec.type == "MQTT" ) {
-		const mqttClient = new MqttClient({url: new URL(`mqtt://${sourceSpec.targetHostname}:${sourceSpec.targetPort}`)});
-		abortSignal.addEventListener("abort", () => {
-			dashboard.log("Disconnecting due to abort signal");
-			mqttClient.disconnect();
-		});
-		await mqttClient.connect();
-		dashboard.connectionStatus = {status: "connected", target: sourceSpec };
-		dashboard.log("Connected to "+formatTargetSpec(sourceSpec)+"!");
-		mqttClient.subscribe("#");
-		mqttClient.on('publish', evt => {
-			dashboard.update(evt.detail.topic, evt.detail.payload);
-		});
-		mqttClient.on("disconnect", () => {
-			dashboard.log("Got disconnect packet, or something from "+formatTargetSpec(sourceSpec)+"!");
-		});
-		mqttClient.on("closed", () => {
-			dashboard.log("Disconnected from "+formatTargetSpec(sourceSpec)+"!");
-			dashboard.connectionStatus = {status: "not-connected"};
-		});
-	} else {
-		throw new Error(`Unrecognized source spec: ${JSON.stringify(sourceSpec)}`)
+class DashboardAppInstance extends AbstractAppInstance<KeyEvent,number> {
+	#abortController = new AbortController();
+	#sourceSpec : TargetSpec;
+	#dashboard : Dashboard;
+	#mqttClient : MqttClient|undefined;
+
+	constructor(sourceSpec:TargetSpec, ctx:PossiblyTUIAppContext) {
+		super(ctx);
+		this.#sourceSpec = sourceSpec;
+		this.#dashboard = new Dashboard(ctx);
+		this.#dashboard.connectionStatus = {status: "connecting", target: sourceSpec };
+		this._connect();
 	}
-
-	const input = Deno.stdin.readable;
-	// abortSignal.addEventListener("abort", () => { input.cancel(); });
-
-	try {
-		Deno.stdin.setRaw(true);
-		dashboard.enterTui();
-		for await(const evt of inputEvents(input)) {
-			dashboard.log(`Read event: ${JSON.stringify(evt)}`);
-			if( evt.key == "\x03" || evt.key == "q" ) {
-				abortController.abort();
-				break;
-			} else if( evt.key == "r" ) { // 'r' for redraw
-				dashboard.log("'r' hit!");
-				dashboard.screenSize = Deno.consoleSize();
-			}
+	
+	_requestCleanExit(result:number) {
+		this.#abortController.abort("Clean exit("+result+")");
+		this._resolve(result);
+	}
+	// deno-lint-ignore no-explicit-any
+	_abort(reason:any) {
+		this._reject(reason);
+	}
+	
+	override handleInput(input:KeyEvent) {
+		this.#dashboard.log(`Key hit: ${JSON.stringify(input)}`);
+		if( input.key == "q" ) {
+			this._requestCleanExit(0);
 		}
-	} catch( e ) {
-		if( e instanceof Error && e.name == 'BadResource' ) {
-			// Presumably from closing the input stream;
-			dashboard.log(`Ignoring error: ${e}`)
+		if(input.key == "c" && input.ctrlKey) {
+			this._abort(new Error("Aborted by user"));
+		}
+	}
+	
+	override _cleanup() {
+		if( this.#mqttClient ) {
+			this.#dashboard.log("Disconnecting for _cleanup");
+			this.#mqttClient.disconnect();
+		}
+		return super._cleanup();
+	}
+	
+	async _connect() : Promise<void> {
+		const sourceSpec = this.#sourceSpec;
+		
+		if( this.#mqttClient ) {
+			this.#dashboard.log("Disconnecting for new connection");
+			this.#mqttClient.disconnect();
+		}
+		
+		if( sourceSpec.type == "MQTT" ) {
+			this.#mqttClient = new MqttClient({url: new URL(`mqtt://${sourceSpec.targetHostname}:${sourceSpec.targetPort}`)});
+			await this.#mqttClient.connect();
+			this.#dashboard.connectionStatus = {status: "connected", target: sourceSpec };
+			this.#dashboard.log("Connected to "+formatTargetSpec(sourceSpec)+"!");
+			this.#mqttClient.subscribe("#");
+			this.#mqttClient.on('publish', evt => {
+				this.#dashboard.update(evt.detail.topic, evt.detail.payload);
+			});
+			this.#mqttClient.on("disconnect", () => {
+				this.#dashboard.log("Got disconnect packet, or something from "+formatTargetSpec(sourceSpec)+"!");
+			});
+			this.#mqttClient.on("closed", () => {
+				this.#dashboard.log("Disconnected from "+formatTargetSpec(sourceSpec)+"!");
+				this.#dashboard.connectionStatus = {status: "not-connected"};
+			});
 		} else {
-			throw e;
+			throw new Error(`Unrecognized source spec: ${JSON.stringify(sourceSpec)}`)
 		}
-	} finally {
-		dashboard.exitTui();
-		Deno.stdin.setRaw(false);
 	}
-	return 0;
 }
 
-const makeDashboardSpawner = (sourceSpec:TargetSpec) => functionToProcessLikeSpawner(dashboardMain(sourceSpec));
+function dashboardAppSpawner(sourceSpec:TargetSpec) : PossiblyTUIAppSpawner<PossiblyTUIAppContext, Waitable<number>, KeyEvent> {
+	return {
+		inputMode: "push-key-events",
+		spawn: ctx => new DashboardAppInstance(sourceSpec, ctx),
+	};
+}
+
+function makeDenoScreenSizeVariable(deno:typeof Deno, initalValue:Vec2D<number>, refreshInterval:number) : WatchableVariable<Vec2D<number>> {
+	return makeReadonlyWatchable(
+		initalValue,
+		(abortSignal, setter) => {
+			const refreshScreenSize = () => {
+				const cs = deno.consoleSize();
+				const vec = {x:cs.columns, y:cs.rows};
+				setter(vec);
+			}
+			
+			let signalListenerAdded : boolean = false;
+			let interval : number|undefined = undefined;
+			
+			refreshScreenSize();
+			
+			try {
+				deno.addSignalListener("SIGWINCH", refreshScreenSize);
+				signalListenerAdded = true;
+			} catch( _e ) {
+				interval = setInterval(refreshScreenSize, refreshInterval);
+			}
+			
+			abortSignal.addEventListener("abort", () => {
+				if(signalListenerAdded) deno.removeSignalListener("SIGWINCH", refreshScreenSize);
+				if(interval) clearInterval(interval);
+			});
+		},
+		vec2dsAreEqual
+	);
+}
+
+function denoTuiAppContext(ctx:ProcessLikeSpawnContext, deno:typeof Deno) : TUIAppRunnerContext {
+	return {
+		stdin : ctx.stdin,
+		stdout: ctx.stdout.writable.getWriter(),
+		outputMode: "screen",
+		screenSizeVar: makeDenoScreenSizeVariable(deno, {x: 40, y: 20}, 1000),
+		registerCleanup: (cb : ()=>void) => {
+			deno.addSignalListener("SIGINT", cb);
+		},
+		unregisterCleanup: (cb : ()=>void) => {
+			deno.removeSignalListener("SIGINT", cb);
+		},
+	};
+}
+
+const makeTuiAppSpawner = (app:PossiblyTUIAppSpawner<PossiblyTUIAppContext,Waitable<number>,KeyEvent>) : ProcessLikeSpawner => {
+	// Wrapper because I haven't yet bothered to
+	// rewrite this whole thing in TUIFramework3 terms
+	return functionToProcessLikeSpawner((ctx, abortSignal) => {
+		// TODO: I guess runTuiApp should take an abortSignal
+		return runTuiApp(app, denoTuiAppContext(ctx, Deno));
+	});
+}
 
 function makeOutputSpawner(outText:string, errText:string, exitCode:number) : ProcessLikeSpawner {
 	const textEncoder = new TextEncoder();
@@ -213,7 +271,7 @@ function parseMain(args:string[]) : ProcessLikeSpawner {
 	if( mqttServer == undefined ) {
 		return makeOutputSpawner("", "No --control-root=... indicated\n", 1);
 	}
-	return makeDashboardSpawner(mqttServer);
+	return makeTuiAppSpawner(dashboardAppSpawner(mqttServer));
 }
 
 if( import.meta.main ) {
